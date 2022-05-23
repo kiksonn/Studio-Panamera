c4b3ced9800486439d8a0b4d999f90f723ed5eaa
Oskar Malinowski <kikson.yy@gmail.com>
2022-05-23 21:10:52 +0200

    fixed nav

--- src/js/main.js
+++ src/js/main.js
@@ -1,17 +1,26 @@
 const navMobile = document.querySelector('.nav-mobile')
 const navBtn = document.querySelector('.hamburger')
 const allNavItems = document.querySelectorAll('.nav__link')
+const toTop = document.querySelector(".top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 250) {
+    toTop.classList.add("top__active");
+  } else {
+    toTop.classList.remove("top__active");
+  }
+})
 
 const handleNav = () => {
     navBtn.classList.toggle('is-active')
     navMobile.classList.toggle('nav-mobile--active')
 
-    // allNavItems.forEach(item => {
-    //     item.addEventListener('click', () => {
-    //         nav.classList.remove('nav--active')
-    //         navBtn.classList.remove('is-active')
-    //     })
-    // }) 
+    allNavItems.forEach(item => {
+        item.addEventListener('click', () => {
+            navMobile.classList.remove('nav-mobile--active')
+            navBtn.classList.remove('is-active')
+        })
+    }) 
 }
 
 navBtn.addEventListener('click', handleNav)
